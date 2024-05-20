4c2b1590cf85c8c03ec1a00731b1ca9f46240898
Akash-Reji <99380160+Akash-Reji@users.noreply.github.com>
2024-05-20 07:28:11 +0530

    Add files via upload

---
+++ pattern12.java
@@ -0,0 +1,11 @@
+class pattern12 {
+	public static void main(String args[]){
+		int c=1;
+		for(int i=1;i<=9;i+=2){
+			for(int j=1;j<=i;j+=2){
+				System.out.print(i);
+				}
+			System.out.println( );
+		}
+	}
+}
